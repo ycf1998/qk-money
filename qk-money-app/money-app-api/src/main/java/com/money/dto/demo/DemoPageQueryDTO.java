7f675b397ce7a3096d561a8b3b2b05ef8d4cec7f
money <374648769@qq.com>
2025-09-20 15:44:07 +0800

    :lock: 修复查询接口 orderBy 参数 SQL 注入漏洞

--- qk-money-app/money-app-api/src/main/java/com/money/dto/demo/DemoPageQueryDTO.java
+++ qk-money-app/money-app-api/src/main/java/com/money/dto/demo/DemoPageQueryDTO.java
@@ -1,9 +1,12 @@
 package com.money.dto.demo;
 
+import com.money.web.util.MoneyCommUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import com.money.web.dto.PageQueryRequest;
+
+import java.util.Map;
 
 /**
 * <p>
@@ -28,4 +31,13 @@ public class DemoPageQueryDTO extends PageQueryRequest {
     */
     private String status;
 
+    /**
+     * 可排序字段映射
+     *
+     * @return {@link Map }<{@link String }, {@link String }>
+     */
+    @Override
+    public Map<String, String> sortKeyMap() {
+        return MoneyCommUtil.sortFieldMap("createTime", "updateTime");
+    }
 }
